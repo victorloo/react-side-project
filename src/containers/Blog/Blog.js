19f951924657f00ecd3a0d0b15dbaa3661158855
victorloo <ila9970@gmail.com>
2018-10-06 22:44:50 +0800

    Simple method for multiple partial at new page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -34,7 +34,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} />*/}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Route path="/posts/:id" exact component={FullPost} />
             </div>
         );
     }
